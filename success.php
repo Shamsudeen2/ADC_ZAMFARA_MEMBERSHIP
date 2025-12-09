a88112d3f5017b7087507f29035d4d87cc8949a2
Shamsudeen2 <166009079+Shamsudeen2@users.noreply.github.com>
2025-12-09 16:12:26 +0100

    fix: success

--- success.php
+++ success.php
@@ -6,7 +6,7 @@
     <title>Registration Successful</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
+jjjjggg
 <body class="p-5 text-center bg-light">
     <div class="container">
         <div class="card p-5 shadow">
